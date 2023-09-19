964640777023c84efa50a4be60abf5e69bda212a
Vladimir Sibirov <trustmaster@kodigy.com>
2023-09-19 21:50:32 +0200

    Fix dark theme logo

--- browser-extension/src/background.js
+++ browser-extension/src/background.js
@@ -4,21 +4,21 @@ function toggleIcons(mode) {
     if (mode === 'dark') {
         browser.action.setIcon({
             path: {
-                16: 'images/icon-dark-16.png',
-                24: 'images/icon-dark-24.png',
-                32: 'images/icon-dark-32.png',
-                48: 'images/icon-dark-48.png',
-                128: 'images/icon-dark-128.png',
+                16: '../images/icon-dark-16.png',
+                24: '../images/icon-dark-24.png',
+                32: '../images/icon-dark-32.png',
+                48: '../images/icon-dark-48.png',
+                128: '../images/icon-dark-128.png',
             }
         });
     } else {
         browser.action.setIcon({
             path: {
-                16: 'images/icon-16.png',
-                24: 'images/icon-24.png',
-                32: 'images/icon-32.png',
-                48: 'images/icon-48.png',
-                128: 'images/icon-128.png',
+                16: '../images/icon-16.png',
+                24: '../images/icon-24.png',
+                32: '../images/icon-32.png',
+                48: '../images/icon-48.png',
+                128: '../images/icon-128.png',
             }
         });
     }
